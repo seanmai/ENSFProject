81e70b89ed12c04e2ff41d3e6ff24655d17c2df8
Sean Mai <seandmai@gmail.com>
2018-04-04 11:29:50 -0600

    Added login GUI

---
+++ ensf_project/src/ensf_project/LoginGUI.java
@@ -0,0 +1,55 @@
+package ensf_project;
+import java.awt.*;
+import javax.swing.*;
+
+public class LoginGUI extends JFrame{
+	JTextField userID;
+	JTextField password;
+	JButton submit;
+	
+	public LoginGUI() {
+		displayLogin();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Change when adding other GUI
+		setSize(400, 400);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
+	}
+	
+	private void displayLogin() {
+		JPanel main = new JPanel();
+		getContentPane().add(main);
+		
+		JPanel loginForm = new JPanel(new GridBagLayout());
+		main.add(loginForm);
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		c.anchor = GridBagConstraints.LINE_END;
+		loginForm.add(new JLabel("User ID: "), c);
+		c.gridy++;
+		loginForm.add(new JLabel("Password: "), c);
+		
+		c.gridy = 0;
+		c.gridx = 1;
+		c.anchor = GridBagConstraints.LINE_START;
+		userID = new JTextField(10);
+		loginForm.add(userID, c);
+		c.gridy++;
+		password = new JTextField(10);
+		loginForm.add(password, c);
+		
+		c.anchor = GridBagConstraints.LINE_END;
+		c.gridy++;
+		submit = new JButton("Login");
+		loginForm.add(submit, c);
+		
+	}
+	public static void main(String[] args) {
+		LoginGUI login = new LoginGUI();
+
+	}
+
+}
